7024e3e7817c6ffc2af566416ca079d98dcc5aac
evilofskyy <matveyboroden@gmail.com>
2026-08-14 10:05:55 +0400

    Add files via upload

---
+++ Message.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+#include <ctime>
+
+class Message {
+private:
+    std::string sender;
+    std::string recipient;
+    std::string text;
+    std::time_t time;
+public:
+    Message(const std::string& _sender, const std::string& _recipient, const std::string& _text, std::time_t _time);
+    std::string getSender() const;
+    std::string getRecipient() const;
+    std::string getText() const;
+    std::time_t getTime() const;
+};
